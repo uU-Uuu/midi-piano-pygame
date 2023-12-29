600dfcc0a3c5eb9b88e8a5284a3845abf3533361
uU-Uuu <yustyna.holubets@gmail.com>
2023-12-29 10:06:16 +0200

    added img.py

---
+++ img.py
@@ -0,0 +1,42 @@
+import pygame
+
+bg_img = pygame.image.load('Assets/imgs/bgImg.png')
+
+close_img = pygame.image.load('Assets/imgs/closeBtn.png')
+close_rect = close_img.get_rect(center=(1185, 60))
+
+back_imag = pygame.image.load('Assets/imgs/back_btn.png')
+back_rect = back_imag.get_rect(center=(1090, 60))
+
+play_img = pygame.image.load('Assets/imgs/playBtn.png')
+play_rect = play_img.get_rect(center=(600, 340))
+
+play_bg =  pygame.image.load('Assets/imgs/playbg.png')
+
+pianobg = pygame.image.load('Assets/imgs/pianoshadbg.png')
+
+piano_img = pygame.image.load('Assets/imgs/piano.png')
+piano_rect = piano_img.get_rect(center=(4, 633))
+
+white_pr_l = pygame.image.load('Assets/imgs/whiteprl.png')
+white_pr_r = pygame.image.load('Assets/imgs/whiteprr.png')
+white_pr_mid = pygame.image.load('Assets/imgs/whiteprmid.png')
+black_pr = pygame.image.load('Assets/imgs/blackpr.png')
+
+watch_img = pygame.image.load('Assets/imgs/watchBtn.png')
+watch_rect = watch_img.get_rect(center=(600, 510))
+
+choosetune_img = pygame.image.load('Assets/imgs/choosetune_btn.png')
+choosetune_rect = choosetune_img.get_rect(center=(590, 290))
+
+playtune_img = pygame.image.load('Assets/imgs/playtune_btn.png')
+playtune_rect = playtune_img.get_rect(center=(590, 493))
+
+choose_img = pygame.image.load('Assets/imgs/rectChoose.png')
+choose_rect = choose_img.get_rect(center=(100, 150))
+
+white_pr_l = pygame.image.load('Assets/imgs/whiteprl.png')
+white_pr_r = pygame.image.load('Assets/imgs/whiteprr.png')
+white_pr_mid = pygame.image.load('Assets/imgs/whiteprmid.png')
+black_pr = pygame.image.load('Assets/imgs/blackpr.png')
+last_pr = pygame.image.load('Assets/imgs/lastpr.png')
